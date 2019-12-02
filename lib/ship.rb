c05009faeaf41b25db09eea2d34ab22fe5a33da7
Steven Anderson <steven.a.anderson2012@gmail.com>
2019-12-02 16:50:29 -0700

    Add ship class shell

---
+++ lib/ship.rb
@@ -0,0 +1,11 @@
+class Ship
+  # def initialize(type, size)
+  #   @hit = false
+  # end
+
+  # def hit
+  #   # unless health <= 0 if hit
+  #   #     reduce health by one
+  #   health -= 1 unless health <= 0
+  # end
+end
