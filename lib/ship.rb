a02cd82879355ca2116eb342c5439e21deda3971
Steven Anderson <steven.a.anderson2012@gmail.com>
2019-12-02 17:51:06 -0700

    Add tests and functionality for sunk?

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,8 +1,24 @@
+require 'pry'
+
 class Ship
-  attr_reader :name, :health
-  
-  def initialize(name, health)
+  attr_reader :name, :ship_length, :health
+
+  def initialize(name, ship_length)
     @name = name
-    @health = health
+    @ship_length = ship_length
+    @health = ship_length
+    @sunk = false
+  end
+
+  def sunk?
+    @sunk
+  end
+
+  def hit
+    if @health > 0
+      @health -= 1
+    else
+      @sunk = true
+    end
   end
 end
--- test/ship_test.rb
+++ test/ship_test.rb
@@ -9,4 +9,34 @@ class ShipTest < Minitest::Test
   def test_cruiser_is_a_ship
     assert_instance_of Ship, @cruiser
   end
+
+  def test_ship_has_attributes
+    assert_equal "Cruiser", @cruiser.name
+    assert_equal 3, @cruiser.ship_length
+    assert_equal 3, @cruiser.health
+  end
+
+  def test_ship_is_not_sunk_by_default
+    assert_equal false, @cruiser.sunk?
+  end
+
+  def test_ship_can_be_hit
+    @cruiser.hit
+
+    assert_equal 2, @cruiser.health
+    assert_equal false, @cruiser.sunk?
+  end
+
+  def test_ship_can_be_sunk_from_hits
+    @cruiser.hit
+    @cruiser.hit
+
+    assert_equal 1, @cruiser.health
+    assert_equal false, @cruiser.sunk?
+
+    @cruiser.hit
+
+    assert_equal 0, @cruiser.health
+    assert_equal true, @cruiser.sunk?
+  end
 end
