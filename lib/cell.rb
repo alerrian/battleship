01b067c138464ecd03052e379e49d9e09a8712d2
Ryan Allen <rcallen89@gmail.com>
2019-12-02 19:25:51 -0700

    Create Fire_Upon method to affect passed Ship Create Test to verify ship changes when Cell is fired upon

--- lib/cell.rb
+++ lib/cell.rb
@@ -2,11 +2,12 @@
 
 
 class Cell
-  attr_reader :coordinate, :ship,
+  attr_reader :coordinate, :ship, :fired_upon
 
   def initialize(coordinate)
     @coordinate = coordinate
     @ship = nil
+    @fired_upon = false
   end
 
   def empty?
@@ -16,5 +17,16 @@ class Cell
 
   def place_ship(ship)
     @ship = ship
+  end
+
+  def fired_upon?
+    @fired_upon
+  end
+
+  def fire_upon
+    if ship != nil
+      @ship.hit
+    end
+    @fired_upon = true
   end
 end
--- test/cell_test.rb
+++ test/cell_test.rb
@@ -27,4 +27,15 @@ class CellTest < MiniTest::Test
     assert_equal @cruiser, @cell.ship
     assert_equal false, @cell.empty?
   end
+
+  def test_cell_can_pass_ship_hit
+    @cell.place_ship(@cruiser)
+
+    assert_equal false, @cell.fired_upon?
+
+    @cell.fire_upon
+
+    assert_equal 2, @cell.ship.health
+    assert_equal true, @cell.fired_upon?
+  end
 end
