85e564672b916401a52b22722a1653a8b067d239
Steven Anderson <steven.a.anderson2012@gmail.com>
2019-12-03 09:47:22 -0700

    Create shell for Board class

--- lib/board.rb
+++ lib/board.rb
@@ -1,18 +1,13 @@
 require './lib/cell'
 
+# Creates the board to play the game
+
 class Board
   def initialize
     
   end
 
   def cells
-    # { A1 => CellObj}
-    #     hash = A1 => Cell(coordinate)
-    range = "A".."D"
 
-    range.to_a
-
-    cell_hash = {}
-    range.each {|key| cell_hash[key] = Cell.new(key)}
   end
 end
