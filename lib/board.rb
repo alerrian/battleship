47d898ff57441665cf59facd48f8ffb9776c141c
Steven Anderson <steven.a.anderson2012@gmail.com>
2019-12-05 17:45:00 -0700

    Refactor for readability

--- lib/board.rb
+++ lib/board.rb
@@ -48,10 +48,7 @@ class Board
     elsif align_verified?(ship_coords) == false
       start_point = @raw_cells_keys.index(ship_coords[0])
       ship_coords == @raw_cells_keys.slice((start_point), (ship_coords.length))
-
-    else
-      false
-
+      
     end
   end
 
